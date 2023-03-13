d0a6bddc4ed79a657f3da8c8085e9fb6503edeaa
InfAurora <albajo0123@gmail.com>
2023-03-12 19:59:39 -0500

    On click add X's to squares

--- photo-bucket/src/index.js
+++ photo-bucket/src/index.js
@@ -3,18 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        value: null,
+    };
+}
+
 render() {
     return (
-    <button className="square">
-        {/* TODO */}
-    </button>
+        <button className="square" onClick={() => { this.setState({value: 'X'}) }}>
+            {this.state.value}
+        </button>
     );
 }
 }
 
 class Board extends React.Component {
 renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
 }
 
 render() {
@@ -24,19 +31,19 @@ render() {
     <div>
         <div className="status">{status}</div>
         <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
         </div>
         <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
         </div>
         <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
         </div>
     </div>
     );
